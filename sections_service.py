bb80dcc6e968adb78e3f832a595faa7b2bc0ac7e
fernjnfv <fernjnfv@gmail.com>
2026-03-13 11:46:12 +0300

    Исправлена проверка возраста.

--- sections_service.py
+++ sections_service.py
@@ -25,7 +25,12 @@ def find_sections(age: int | None, sport: str | None):
         sport_ok = True
 
         if age is not None:
-            age_ok = section["age_from"] <= age <= section["age_to"]
+            if section["age_to"] is not None and section["age_from"] is not None:
+                age_ok = section["age_from"] <= age <= section["age_to"]
+            elif section["age_to"] is not None:
+                age_ok = age <= section["age_to"]
+            elif section["age_from"] is not None:
+                age_ok = section["age_from"] <= age
 
         if sport_query:
             sport_ok = sport_query in normalize_text(section["sport"])
